6b9363c0740b25f669c070f8d97c40390f22e848
Steffen Aadnevik <steffen.aadnevik@skatteetaten.no>
2026-03-24 11:32:26 +0100

    more

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello from Railway, Steffen";
+        return "Hello from Railway, Steffen test";
     }
 }
